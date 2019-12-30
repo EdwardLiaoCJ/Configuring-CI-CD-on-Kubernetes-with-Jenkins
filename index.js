b6791d7435123b861a6c5c58ed44949069e92e56
EdwardLiaoCJ <liaocj@cn.ibm.com>
2019-12-30 15:17:06 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var app = express();
 
 app.get('/getpodinfo', function (req, res) {
     const podInfo = {
-        "MY_NODE_NAME": process.env.MY_NODE_NAME || '',
+        "MY_NODE_NAME_IBM Cloud": process.env.MY_NODE_NAME || '',
         "MY_POD_NAME": process.env.MY_POD_NAME || '',
         "MY_POD_NAMESPACE": process.env.MY_POD_NAMESPACE || '',
         "MY_POD_IP": process.env.MY_POD_IP || '',
